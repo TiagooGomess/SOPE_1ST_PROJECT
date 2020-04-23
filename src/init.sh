f8a91c66744724ec6e1fd36d83e3b407515455ac
TiagooGomess <tiagogomes2011@live.com.pt>
2020-04-23 20:08:02 +0100

    Cleaned repository

--- src/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export LOG_FILENAME=log.txt
-
-gcc -Wall -g simpledu.c -lm -o simpledu
-
-#./simpledu -l . -B kB -a -L --max-depth=3 # se correr este comando no terminal, a variável de ambiente fica LOG_FILENAME fica a NULL... Temos que ver como resolver isto.
--- src/teste.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-(./simpledu -l ../TestDir -L -a -B19 --count-links -S || echo $?) | sort -k2 > testeT_simpledu.txt
-(du -l ../TestDir -L -a -B19 --count-links -S || echo $?) | sort -k2 > testeT_du.txt
-diff -q testeT_simpledu.txt testeT_du.txt > /dev/null 2>&1 && echo OK || echo FAILED
